3cd6a5c4b817ed4fe572dad5de04fda0a50dd836
nicugnm <nicolae.ghergu@s.unibuc.ro>
2022-05-25 20:58:55 +0300

    Fix build

--- src/test/java/ro/nicolaemariusghergu/easylearn/datamanager/DataManagerApplicationTests.java
+++ src/test/java/ro/nicolaemariusghergu/easylearn/datamanager/DataManagerApplicationTests.java
@@ -1,10 +1,10 @@
-package ro.nicolaemariusghergu.easylearn.books;
+package ro.nicolaemariusghergu.easylearn.datamanager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BooksApplicationTests {
+class DataManagerApplicationTests {
 
     @Test
     void contextLoads() {
